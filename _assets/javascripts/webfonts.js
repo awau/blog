39abc26229c46798c106e7e95f7d5b47d7f24ea4
Dean Sheather <dean@deansheather.com>
2017-12-02 20:40:15 +1000

    initial commit

---
+++ _assets/javascripts/webfonts.js
@@ -0,0 +1,8 @@
+//= require webfontloader/webfontloader.js
+
+WebFont.load({
+  google: {
+    //families: ['Cormorant Garamond:700', 'Lato:300,400,700']
+    families: ['Roboto Mono:400,400i,700,700i']
+  }
+});
